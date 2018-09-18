b60cf74024b81aa1eba04de6854b93254eb0b97f
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-09-18 17:46:42 +0300

    add initial connection class with open con and close con

---
+++ metadataGenerator/ConnectionSQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace metadataGenerator
+{
+    public class ConnectionSQL
+    {
+        string connectionString = "Data Source=192.168.30.138;" +
+                                    "Initial Catalog=TUES;" +
+                                    "User id=sa;" +
+                                    "Password=Ankara123;";
+        SqlConnection con;
+
+        public void openConnection()
+        {
+            con = new SqlConnection(connectionString);
+            con.Open();
+        }
+
+        public void closeConnection()
+        {
+            con.Close();
+        }
+    }
+}
--- metadataGenerator/Program.cs
+++ metadataGenerator/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml;
 
+
 namespace metadataGenerator
 {
     class Program
