9463cd3b11daa2378fe7090911c4066d3ad8f10f
Arda Çetinkaya <ctnkyrd@gmail.com>
2019-03-11 15:37:19 +0300

    change progressbar length

--- metadataGenerator/ProgressBar.cs
+++ metadataGenerator/ProgressBar.cs
@@ -7,7 +7,7 @@ using System.Threading;
 /// </summary>
 public class ProgressBar : IDisposable, IProgress<double>
 {
-    private const int blockCount = 10;
+    private const int blockCount = 30;
     private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
     private const string animation = @"|/-\";
 
